409d305a6e6abb61987b81cfeb385d2e68303e13
Aritra <aritra1521@gmail.com>
2026-01-11 00:03:35 +0530

    fix:change process.env to import.meta.env

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { genericOAuthClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: `${process.env.VITE_API_URL}/api/auth`, // The base URL of your auth server
+  baseURL: `${import.meta.env.VITE_API_URL}/api/auth`, // The base URL of your auth server
   plugins: [genericOAuthClient()],
 });
 
